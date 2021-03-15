55ca7b41ae5aec2cb9ac742152ae00dc1a9adac7
Trucmuche88 <elenazhiro@MacBook-Air-Elena.local>
2021-03-15 22:14:23 +0300

    ДЗ lesson-3 (cart)

--- ex-1/script.js
+++ ex-1/script.js
@@ -1,155 +1,124 @@
+class ApiMock {
+  constructor() {
+
+  }
+
+  fetch() {
+    return [
+        { title: 'Shirt', price: 150 },
+        { title: 'Socks', price: 50 },
+        { title: 'Jacket', price: 350 },
+        { title: 'Shoes', price: 250 },
+      ];
+  }
+}
+
 class GoodsItem {
   constructor(title, price) {
     this.title = title;
     this.price = price;
   }
-  render() {
-      return `<div class="goods-item"><div class="article"><h3 class="product">${this.title}</h3><p class="price">${this.price}</p></div><div class="goods-button"><button class="gd-bt">Добавить</button></div></div></div>`;
 
+  getHtml() {
+    return `<div class="goods-item"><div class="article"><h3 class="product">${this.title}</h3><p class="price">${this.price}</p></div><div class="goods-button"><button class="gd-bt">Добавить</button></div></div></div>`;
   }
 }
 
 class GoodsList {
   constructor() {
+    this.api = new ApiMock();
+    this.$goodsList = document.querySelector('.goods-list');
     this.goods = [];
   }
+
   fetchGoods() {
-    this.goods = [
-      { title: 'Shirt', price: 150 },
-      { title: 'Socks', price: 50 },
-      { title: 'Jacket', price: 350 },
-      { title: 'Shoes', price: 250 },
-    ];
+    this.goods = this.api.fetch().map(({title, price}) => new GoodsItem(title, price));
   }
+
   render() {
-    let listHtml = '';
-    this.goods.forEach(good => {
-      const goodItem = new GoodsItem(good.title, good.price);
-      listHtml += goodItem.render();
-    });
-    document.querySelector('.goods-list').innerHTML = listHtml;
+    this.$goodsList.textContent = '';
+    this.goods.forEach((good) => {
+        this.$goodsList.insertAdjacentHTML('beforeend', good.getHtml());
+    })
   }
-  total() {
-    let total = 0;
+}
 
-    this.goods.forEach(good => {
-      total += good.price;
-    });
+const goodsList = new GoodsList();
 
-    return total;
+goodsList.fetchGoods();
+goodsList.render();
+
+class Cart {
+  constructor() {
+    this.items = [];
+    this.total = 0; 
   }
-} 
 
-  const list = new GoodsList();
-  list.fetchGoods();
-  list.render();
-  console.log(list.total());
-  
-  class Cart {
-    constructor() {
-      this.items = [];
-      this.total = 0; 
-    }
+  addItem(item) {
+    let existingItem = this.items.find(element => element.title === item.title);
 
-    addItem(item) {
+    if (existingItem === undefined) {
       this.items.push(item);
-      this.total += item.price; 
+    } else {
+      existingItem.quantity++;
     }
-  };
 
-  class CartItem {
-    constructor(title, price, quantity = 1) {
-      this.title = title;
-      this.price = price;
-      this.quantity = quantity;
-    }
-  };
+    cart.refreshTotal();
+  }
 
-  let item1 = new CartItem("Product-1", 50);
-  let item2 = new CartItem("Product-2", 250);
+  removeItem(title) {
+    this.items.splice(this.items.findIndex(item => item.title === title), 1);
+    cart.refreshTotal();
+  }
+  
+  refreshTotal() {
+    this.total = 0;
+    this.items.forEach(item => this.total += item.quantity * item.price);
+  }
 
-  let cart = new Cart();
-  cart.addItem(item1);
-  cart.addItem(item2);
+  getItems() {
+    return this.items;
+  }
+};
 
-  console.log(cart.items);
-  console.log(cart.total);
-
-/*
-
-class Burger {
-  constructor(price, calories) {
+class CartItem {
+  constructor(title, price, quantity = 1) {
+    this.title = title;
     this.price = price;
-    this.calories = calories;
-    this.topping = [];
-  }
-
-  addTopping(topping) {
-    this.topping.push(topping);
-    this.price += topping.price;
-    this.calories += topping.calories;
+    this.quantity = quantity;
   }
 }
 
-class Toppings {
-  constructor(price, calories) {
-    this.price = price;
-    this.calories = calories;
-  }
+
+let cart = new Cart();
+cart.addItem(new CartItem("Product-1", 50));
+cart.addItem(new CartItem("Product-2", 250));
+cart.addItem(new CartItem("Product-2", 250)); 
+cart.addItem(new CartItem("Product-2", 250));
+cart.removeItem("Product-1");
+
+console.table(cart);
+
+
+
+function makeGetRequest(url) {
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", url);
+    xhr.onload = function () {
+      resolve(xhr.responseText);
+    };
+    xhr.onerror = function () {
+      reject(xhr.status);
+    };
+    xhr.send();
+  });
 }
 
-class Cheese extends Toppings {
-  constructor() {
-    super(10, 20);
-  }
-}
-
-class Salad extends Toppings {
-  constructor () {
-    super(10, 5);
-  }
-}
-
-class Potatoes extends Toppings {
-  constructor() {
-    super(15, 10);
-  }
-}
-
-class Spices extends Toppings {
-  constructor() {
-    super(15, 0);
-  }
-}
-
-class Mayo extends Toppings {
-  constructor() {
-    super(20, 5);
-  }
-}
-
-class BigBurger extends Burger {
-  constructor() {
-    super(100, 40);
-  }
-}
-
-class SmallBurger extends Burger {
-  constructor() {
-    super(50, 20);
-  }
-}
-
-// test of a burger
-
-let smallBurger = new SmallBurger();
-smallBurger.addTopping(new Cheese());
-smallBurger.addTopping(new Cheese());
-smallBurger.addTopping(new Potatoes());
-smallBurger.addTopping(new Mayo());
-smallBurger.addTopping(new Salad());
-smallBurger.addTopping(new Spices());
-
-console.log(smallBurger.price, smallBurger.calories, smallBurger.topping);
-console.table(smallBurger);
-*/
+makeGetRequest('https://baconipsum.com/api/?type=meat-and-filler').then(function(data) {
+  console.log('Success');
+  console.log(data);
+}).catch(function(data) {
+  console.log('Fail');
+  console.log(data);
+});
